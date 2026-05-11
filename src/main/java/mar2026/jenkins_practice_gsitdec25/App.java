4e608a533fa82cd3276574bf516939d921c81677
priyanka <priyankarr140@gmail.com>
2026-05-11 21:23:22 +0530

    initial commit

---
+++ src/main/java/mar2026/jenkins_practice_gsitdec25/App.java
@@ -0,0 +1,13 @@
+package mar2026.jenkins_practice_gsitdec25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mar2026/jenkins_practice_gsitdec25/LoginTest.java
@@ -0,0 +1,10 @@
+package mar2026.jenkins_practice_gsitdec25;
+import org.testng.annotations.*;
+public class LoginTest {
+	@Test
+	void hello()
+	{
+		System.out.println("LOGIN TEST IS WORKING****");
+	}
+
+}
